41f2bc552504e098a4a71f9a3fc2ce0796f43ade
Zestinc <zestinc32@gmail.com>
2023-10-26 16:11:28 -0700

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,8 +40,8 @@ subprojects {
             minSdk = 21
             targetSdk = 33
 
-            versionName = "1.0.1"
-            versionCode = 100001
+            versionName = "1.0.2"
+            versionCode = 100002
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
