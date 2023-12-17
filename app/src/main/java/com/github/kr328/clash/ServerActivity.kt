90aa328b8cc0e35dbba3ffcff7b4cf5da81c967c
Zestinc <zestinc32@gmail.com>
2023-12-16 22:04:02 -0800

    exception fallback

--- app/src/main/java/com/github/kr328/clash/ServerActivity.kt
+++ app/src/main/java/com/github/kr328/clash/ServerActivity.kt
@@ -9,8 +9,16 @@ import kotlinx.serialization.json.Json
 
 class ServerActivity : BaseActivity<ServerDesign>() {
     override suspend fun main() {
-        val serverMetadataJson = fetchStringUrlFromLambda(this, "https://3sm6xoow37.execute-api.us-east-2.amazonaws.com/airport_server_metadata_1")
-        val serverMetadata = Json.decodeFromString(ServerMetadata.serializer(), serverMetadataJson)
+        val serverMetadata = try {
+            val serverMetadataJson = fetchStringUrlFromLambda(this, "https://3sm6xoow37.execute-api.us-east-2.amazonaws.com/airport_server_metadata_1")
+            Json.decodeFromString(ServerMetadata.serializer(), serverMetadataJson)
+        } catch (error: Exception) {
+            ServerMetadata(
+                title = "Cloud Yu",
+                subtitle = "A server with limited English language support 🥹\n点击跳转，推荐“中转套餐”",
+                url = "https://cloudyu.top/#/register?code=ycOEE8sN"
+            )
+        }
         val design = ServerDesign(this, serverMetadata) {
             startActivity(Intent(Intent.ACTION_VIEW).setData(it))
         }
